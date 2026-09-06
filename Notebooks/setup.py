64842fb2fe69c625548fe3d6a74609d9faa1f880
Olunoyo <ninitouch1soul@yahoo.com>
2026-09-06 14:10:07 +0100

    data cleaning to ml

---
+++ Notebooks/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "fraudulent_transaction_detection",
+    version = "1.0.0",
+    author= "olunoyo okorodudu",
+    author_email= "arieanice3@gmail.com",
+    packages = find_packages(), 
+)
